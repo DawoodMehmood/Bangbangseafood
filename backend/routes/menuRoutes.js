a9e22910c16485d8122ebd253afc5bd79e8a95d1
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-10 20:21:16 +0500

    Refactor menuRoutes.js to use new gallery image endpoints

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,39 +1,15 @@
-// routes/contactRoutes.js
-
+// menuRoutes.js
 const express = require("express");
 const {
-  createCategory,
-  getAllCategories,
-  updateCategory,
-  deleteCategory,
-  addDish,
-  updateDish,
-  deleteDish,
+  createGalleryImage,
+  getAllGalleryImages,
+  deleteGalleryImageById,
 } = require("../controllers/menuController");
 
 const menuRouter = express.Router();
 
-// Create a new category with dishes
-menuRouter.post("/categories", createCategory);
-
-// Get all categories and their dishes
-menuRouter.get("/categories", getAllCategories);
-
-// Update a category and its dishes
-menuRouter.put("/categories/:categoryId", updateCategory);
-
-// Delete a category
-menuRouter.delete("/categories/:categoryId", deleteCategory);
-
-
-
-// Add a new dish to an existing category
-menuRouter.post('/categories/:categoryId/dishes', addDish);
-
-// Update the details of a specific dish in a category
-menuRouter.put("/categories/:categoryId/dishes/:dishId", updateDish);
-
-// Delete a specific dish in a category
-menuRouter.delete("/categories/:categoryId/dishes/:dishId", deleteDish);
+menuRouter.post("/newImage", createGalleryImage);
+menuRouter.get("/getAllImages", getAllGalleryImages);
+menuRouter.delete("/deleteImage/:id", deleteGalleryImageById);
 
 module.exports = menuRouter;
