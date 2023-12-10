36ef4891b775a10f8a1b01217b619989c03c92de
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-10 20:20:35 +0500

    Made new menu schema

--- backend/models/menuModel.js
+++ backend/models/menuModel.js
@@ -1,12 +1,17 @@
+// menuModel.js
 const mongoose = require("mongoose");
 
-const menuSchema = new mongoose.Schema(
-  {
-    image: {
-      type: String,
-    },
-  }
-);
+const menuSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+  sequenceNo: {
+    type: Number,
+    required: true,
+  },
+});
 
-const Menu = mongoose.model("menu", menuSchema);
-module.exports = Menu;
+const MenuImages = mongoose.model("menuImages", menuSchema);
+
+module.exports = MenuImages;
