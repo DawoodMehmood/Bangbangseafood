bd67c50ce64f537a0a8daf9c9c67606d860e6653
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-10 20:18:04 +0500

    Add body-parser middleware for parsing requests

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const connectDB = require("./config/db");
 const colors = require("colors");
 const express = require("express");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const contactRoutes = require("./routes/contactRoutes");
 const menuRoutes = require("./routes/menuRoutes");
-const imageRoutes = require("./routes/imageRoutes");
 const bannerRoutes = require("./routes/bannerRoutes");
 const credentialRoutes = require("./routes/credentialRoutes");
 
@@ -18,13 +18,18 @@ const app = express();
 // CORS to allow requests
 app.use(cors());
 
+// Parse application/json requests
+app.use(bodyParser.json({ limit: "50mb" }));
+
+// Parse application/x-www-form-urlencoded requests
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
 //middleware
 app.use(express.json());
 
 // routes
 app.use("/api/contact", contactRoutes);
 app.use("/api/menu", menuRoutes);
-app.use("/api/image", imageRoutes);
 app.use("/api/banner", bannerRoutes);
 app.use("/api/credential", credentialRoutes);
 
