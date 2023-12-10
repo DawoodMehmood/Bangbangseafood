950fe654dca793daa0c06d5942a6c9035579ce1f
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-10 20:18:22 +0500

    Refactor db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,24 +2,18 @@
 // import colors from "colors"
 // import dotenv from "dotenv"
 
-const mongoose = require('mongoose')
-const colors = require('colors')
-const dotenv = require('dotenv')
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-            const conn = await mongoose.connect(process.env.MONGO_URI);
-            console.log(`Connected to MongoDB ${conn.connection.host}`.bgMagenta.white);
-
-    }
-    catch (error){
-        console.log(`Error in running MongoDB ${error}`.bgRed.white);
-    }
-}
-
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to MongoDB ${conn.connection.host}`.bgMagenta.white);
+  } catch (error) {
+    console.log(`Error in running MongoDB ${error}`.bgRed.white);
+  }
+};
 
 module.exports = connectDB;
-
-
-
