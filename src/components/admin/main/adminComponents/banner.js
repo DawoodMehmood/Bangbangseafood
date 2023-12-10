32d1b9a92f65e4a08837b27a6d6caaf5ee2bcb71
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-10 20:19:06 +0500

    Fix error handling and remove unnecessary console.log

--- src/components/admin/main/adminComponents/banner.js
+++ src/components/admin/main/adminComponents/banner.js
@@ -52,15 +52,12 @@ const BannerComponent = () => {
     })
       .then((response) => {
         if (!response.ok) {
+          showToast("Error Updating Record", "error");
           throw new Error("Network response was not ok");
         }
         showToast("Record Updated Successfully", "success");
         setCheckFormData(formData);
         return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // Optionally, perform any action upon successful update
       })
       .catch((error) => {
         console.error("Error updating banner:", error);
