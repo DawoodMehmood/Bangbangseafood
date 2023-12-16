116bf6e3bd3ded7a0aee8a7f82d90fb186139d47
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-16 16:09:31 +0500

    Refactor order-online and pre-order components

--- src/components/order-online/order-online.js
+++ src/components/order-online/order-online.js
@@ -1,20 +1,15 @@
-// ... Other imports ...
-
 import React from "react";
-import {  Container, Row, Button } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
 import "./../contact/contact.css";
 import "./order-online.css";
 
 const Menu = () => {
-  const handleButtonClick = () => {
-    window.open("https://www.clover.com/online-ordering/bang-bang-seafood-west-palm-beach", "_blank");
-  };
   return (
     <div>
       <div className="contact-image">
         <img
           src="https://as1.ftcdn.net/v2/jpg/01/07/76/96/1000_F_107769633_FrmulZCjEzdZ46f5LGbx26JmSuXdCILH.jpg"
-          alt="Contact Us Image"
+          alt="header img"
           loading="lazy"
         />
         <h1 className="fs-1 centered-heading">ONLINE PICK-UP ORDER</h1>
@@ -22,14 +17,16 @@ const Menu = () => {
 
       <Container>
         <Row>
-          <div className="pre-order text-center my-5">
+          <div className="pre-order text-center marginTopBelow">
             <p>
               Contact us at 561-847-4078 to place your online pick-up order
-              <br /> or by clicking the button below.
+              <br /> or by clicking the button below &darr;
             </p>
-            <Button className="p-3 mt-3" variant="warning" onClick={handleButtonClick}>
-              PLACE YOUR PICK-UP ORDERS HERE
-            </Button>
+            <a href="https://www.clover.com/online-ordering/bang-bang-seafood-west-palm-beach">
+              <Button className="p-2 mt-3" variant="warning">
+                PLACE YOUR ORDER HERE
+              </Button>
+            </a>
           </div>
         </Row>
       </Container>
--- src/components/pre-order/pre-order.js
+++ src/components/pre-order/pre-order.js
@@ -1,36 +1,31 @@
-// ... Other imports ...
-
 import React from "react";
-import {  Container, Row, Col, Button } from "react-bootstrap";
-import logo from "./../../img/logo.png";
-
+import { Container, Row, Button } from "react-bootstrap";
 import "./pre-order.css";
 
 const PreOrder = () => {
-  const handleButtonClick = () => {
-    window.open("https://www.clover.com/online-ordering/bang-bang-seafood-west-palm-beach", "_blank");
-  };
   return (
     <div>
       <div className="contact-image">
         <img
           src="https://as1.ftcdn.net/v2/jpg/01/07/76/96/1000_F_107769633_FrmulZCjEzdZ46f5LGbx26JmSuXdCILH.jpg"
           loading="lazy"
-          alt="Contact Us Image"
+          alt="header img"
         />
         <h1 className="fs-1 centered-heading">PRE ORDER</h1>
       </div>
 
       <Container>
         <Row>
-          <div className="pre-order text-center my-5">
+          <div className="pre-order text-center marginTopBelow">
             <p>
               Contact us at 561-847-4078 to place your pre-order
-              <br /> or by clicking the button below.
+              <br /> or by clicking the button below &darr;
             </p>
-            <Button className="p-3 mt-3" variant="warning" onClick={handleButtonClick}>
-              PLACE YOUR PRE-ORDERS HERE
-            </Button>
+            <a href="https://www.clover.com/online-ordering/bang-bang-seafood-west-palm-beach">
+              <Button className="p-2 mt-3" variant="warning">
+                PLACE YOUR ORDER HERE
+              </Button>
+            </a>
           </div>
         </Row>
       </Container>
