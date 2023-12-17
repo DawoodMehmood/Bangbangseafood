b14ab793fa75b13578abc1f2f19bd5a26abaf293
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-17 13:57:43 +0500

    Add adminChangePassword route and middleware

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { adminLogin, adminSignUp } = require("../controllers/adminController");
+const {
+  adminLogin,
+  adminSignUp,
+  adminChangePassword,
+} = require("../controllers/adminController");
 const adminRouter = express.Router();
+const middleware = require("../middleware/jwtMiddleware");
 
 adminRouter.post("/login", adminLogin);
 adminRouter.post("/signup", adminSignUp);
+adminRouter.post("/change-password", middleware, adminChangePassword);
 
 module.exports = adminRouter;
