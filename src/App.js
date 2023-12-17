b9b75ce5a765119faa90d5a6535386f0f82b99de
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-17 13:54:49 +0500

    Add token validation and protected route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/homepage";
 import Contact from "./components/contact/contact";
@@ -13,22 +13,84 @@ import AdminLayout from "./layouts/adminLayout";
 import MainLayout from "./layouts/mainLayout";
 import AdminHome from "./components/admin/main/adminHome";
 import "./App.css";
+import BACKEND_URL from "./config";
 
 function App() {
+  // Function to check if the token is expired
+  const isTokenExpired = (token) => {
+    try {
+      const { exp } = JSON.parse(atob(token.split(".")[1]));
+      return exp < Date.now() / 1000;
+    } catch {
+      return true;
+    }
+  };
+
+  const isAuthenticated = async () => {
+    const token = sessionStorage.getItem("token");
+    if (!token || isTokenExpired(token)) {
+      return false;
+    }
+
+    try {
+      const response = await fetch(`${BACKEND_URL}/api/token/validate`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return response.ok;
+    } catch (error) {
+      console.error("Error validating token:", error);
+      sessionStorage.removeItem("token");
+      return false;
+    }
+  };
+
+  // ProtectedRoute component
+  const ProtectedRoute = ({ children }) => {
+    const [isAuth, setIsAuth] = useState(null);
+
+    useEffect(() => {
+      const checkAuth = async () => {
+        const authStatus = await isAuthenticated();
+        setIsAuth(authStatus);
+      };
+
+      checkAuth();
+    }, []);
+
+    if (isAuth === null) {
+      return <div>Loading...</div>; // or some loading component
+    }
+
+    return isAuth ? (
+      children
+    ) : (
+      <Navigate to="/bangbangseafood/controlUddaycontrol/controlpanel" />
+    );
+  };
+
   return (
     <Routes>
       {/* Admin Panel Routes */}
       <Route
         path="/bangbangseafood/controlUddaycontrol/controlpanel/*"
         element={
-          <>
-            <AdminLayout>
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/adminhome" element={<AdminHome />} />
-              </Routes>
-            </AdminLayout>
-          </>
+          <AdminLayout>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/adminhome"
+                element={
+                  <ProtectedRoute>
+                    <AdminHome />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </AdminLayout>
         }
       />
 
